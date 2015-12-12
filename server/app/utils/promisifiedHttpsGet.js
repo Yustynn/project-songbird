cdd550b9417ad7151a146ca2bae02258ae00d7e2
Yustynn <yustynn@gmail.com>
2015-12-12 16:39:38 -0500

    Implement untested promisified HTTP get function

---
+++ server/app/utils/promisifiedHttpsGet.js
@@ -0,0 +1,19 @@
+var http = require('https');
+var Promise = require('bluebird');
+
+function promisifiedHttpsGet(url) {
+    return new Promise(function(resolve, reject) {
+        https.get(url, function(res) {
+            var body = '';
+            res.on('data', function() {
+                body += data;
+            });
+            res.on('end', function() {
+                resolve(body);
+            })
+            res.on('error', reject);
+        })
+    });
+}
+
+module.exports = promisifiedHttpsGet;
